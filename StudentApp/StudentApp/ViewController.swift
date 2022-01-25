ec05269e862db07f8f3112b9a50145681a5d91e0
Chaitanya Punaati <89426302+ChaitanyaPunaati@users.noreply.github.com>
2022-01-25 15:29:55 -0600

    Add files via upload

---
+++ StudentApp/StudentApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudentApp
+//
+//  Created by Punaati,Chaitanya on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameOutlet: UITextField!
+    
+    @IBOutlet weak var courseOutlet: UITextField!
+    
+    @IBOutlet weak var subjectsOutlet: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
