576d929bb38609ae55bc6d37f938812af690680a
Chaitanya Punaati <89426302+ChaitanyaPunaati@users.noreply.github.com>
2022-03-29 15:20:42 -0500

    tables file uploaded

---
+++ tableViewDemo/tableViewDemo/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  tableViewDemo
+//
+//  Created by Punaati,Chaitanya on 3/29/22.
+//
+
+import UIKit
+
+class Product {
+    var productName : String?
+    var productCategory : String?
+    
+    init(productName : String, productCategory : String) {
+        self.productName = productName
+        self.productCategory = productCategory
+    }
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return productsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Create cell
+        var cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
+        //Assign data to the cell
+        cell.textLabel?.text = productsArray[indexPath.row].productName
+        //Return cell
+        return cell
+    }
+    
+    //Create an array
+    var productsArray = [Product]()
+    
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
+        let product1 = Product(productName: "MacBookAir", productCategory: "Laptop")
+        productsArray.append(product1)
+        
+        let product2 = Product(productName: "iPhone", productCategory: "Mobile Phone")
+        productsArray.append(product2)
+        
+        let product3 = Product(productName: "Airpods", productCategory: "Accessories")
+        productsArray.append(product3)
+        
+        let product4 = Product(productName: "iWatch", productCategory: "Accesseries")
+        productsArray.append(product4)
+        
+        let product5 = Product(productName: "iPad", productCategory: "Tablet")
+        productsArray.append(product5)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let transition = segue.identifier
+        if transition == "detailsSegue" {
+            let destination = segue.destination as! DetailsViewController
+            
+            destination.product = productsArray[(tableViewOutlet.indexPathForSelectedRow?.row)!]
+        }
+    }
+
+
+}
+
